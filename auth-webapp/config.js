54bac84d155d2fbf63d81531eb139c4ae8d8a200
caliperce <0109aishwarya.mxd@gmail.com>
2025-04-15 13:35:37 +0530

    change to host in vercel

--- auth-webapp/config.js
+++ auth-webapp/config.js
@@ -11,7 +11,17 @@ const config = {
     isCollege: () => window.location.hostname === '192.168.90.127',
     
     // Test if we're in the home environment
-    isHome: () => window.location.hostname === '192.168.0.123'
+    isHome: () => window.location.hostname === '192.168.0.123',
+
+    firebase: {
+        apiKey: window.ENV_FIREBASE_API_KEY || process.env.FIREBASE_API_KEY,
+        authDomain: window.ENV_FIREBASE_AUTH_DOMAIN || process.env.FIREBASE_AUTH_DOMAIN,
+        projectId: window.ENV_FIREBASE_PROJECT_ID || process.env.FIREBASE_PROJECT_ID,
+        storageBucket: window.ENV_FIREBASE_STORAGE_BUCKET || process.env.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: window.ENV_FIREBASE_MESSAGING_SENDER_ID || process.env.FIREBASE_MESSAGING_SENDER_ID,
+        appId: window.ENV_FIREBASE_APP_ID || process.env.FIREBASE_APP_ID,
+        measurementId: window.ENV_FIREBASE_MEASUREMENT_ID || process.env.FIREBASE_MEASUREMENT_ID
+    }
 };
 
 export default config; 
--- auth-webapp/firebase.js
+++ auth-webapp/firebase.js
@@ -10,17 +10,10 @@ import {
     addDoc
 } from "https://www.gstatic.com/firebasejs/11.3.0/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.3.0/firebase-auth.js";
+import config from './config.js';
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID
-};
+const firebaseConfig = config.firebase;
 
 // Initialize Firebase
 console.log("Initializing Firebase...");
